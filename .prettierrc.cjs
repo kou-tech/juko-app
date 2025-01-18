ae893ebaa7e9fe8bcf9d5f44fd81954b27b4f6de
Kou <hikkappi@yahoo.co.jp>
2025-01-18 20:10:23 +0900

    Add : prettierを導入。

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  "plugins": ["prettier-plugin-tailwindcss"],
+  "printWidth": 80,
+  "useTabs": false,
+  "tabWidth": 2,
+  "singleQuote": true,
+  "bracketSameLine": false,
+  "bracketSpacing": true,
+  "semi": true,
+  "embeddedLanguageFormatting": "auto"
+}
